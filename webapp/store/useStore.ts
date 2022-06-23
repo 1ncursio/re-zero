e40951ba0b29e545be1beabc7aa7b96a441c6144
1ncursio <ckswn1323@naver.com>
2022-06-24 03:49:42 +0900

    [Modify] zustand devtools 재활성화

--- webapp/store/useStore.ts
+++ webapp/store/useStore.ts
@@ -1,7 +1,7 @@
 import { enableMapSet } from 'immer';
 import { mountStoreDevtool } from 'simple-zustand-devtools';
 import create, { GetState, SetState } from 'zustand';
-// import { devtools } from 'zustand/middleware';
+import { devtools } from 'zustand/middleware';
 // import { immer } from 'zustand/middleware/immer';
 import createConfigSlice, { ConfigSlice } from './configSlice';
 import createReversiSlice, { ReversiSlice } from './reversiSlice';
@@ -16,16 +16,16 @@ export type AppSlice<T> = (set: SetState<AppState>, get: GetState<AppState>) => 
  * 슬라이스를 각각 생성하여 모두 합친 스토어를 만듦.
  */
 const useStore = create<AppState>(
-  // devtools(
-  // persist(
-  (set, get) => ({
-    ...createConfigSlice(set, get),
-    ...createReversiSlice(set, get),
-  }),
-  // {
-  //   enabled: process.env.NODE_ENV === 'development',
-  // },
-  // ),
+  devtools(
+    // persist(
+    (set, get) => ({
+      ...createConfigSlice(set, get),
+      ...createReversiSlice(set, get),
+    }),
+    {
+      enabled: process.env.NODE_ENV === 'development',
+    },
+  ),
   //   {
   //     name: 'reversi',
   //   },
